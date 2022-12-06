78a469afcddf4eabb5760089e2c5e17cb80b42ad
nicolai-hornung-bl <nicolai.hornung@brainlab.com>
2022-12-06 09:17:19 +0100

    moved functionality into method

--- door-6/solution.py
+++ door-6/solution.py
@@ -1,12 +1,10 @@
+def findPacket(chars, length):
+    for c in list(enumerate(chars))[:-length]:
+        if len(set(chars[c[0]:c[0]+length])) == length:
+            return (c[0]+length, chars[c[0]:c[0]+length])
+
 with open('door-6/input.txt', 'r') as input:
-    marker = set()
     chars = input.read()
     input.close()
-    for c in list(enumerate(chars))[:-4]:
-        if len(set(chars[c[0]:c[0]+4])) == 4:
-            print(f"start-of-packet loc: {c[0]+4} marker: {chars[c[0]:c[0]+4]}")
-            break
-    for c in list(enumerate(chars))[:-14]:
-        if len(set(chars[c[0]:c[0]+14])) == 14:
-            print(f"start-of-message loc: {c[0]+14} marker: {chars[c[0]:c[0]+14]}")
-            break
+    print(f"start-of-packet {findPacket(chars, 4)}")
+    print(f"start-of-message {findPacket(chars, 14)}")
